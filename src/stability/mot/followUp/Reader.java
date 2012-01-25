521879025a609d2536609c04234ed00716553eba
jefferson <jefferson.almeida.comp@gmail.com>
2012-01-25 20:49:19 -0300

    achei as linhas responsaveis por tombstones.

--- src/stability/mot/followUp/Reader.java
+++ src/stability/mot/followUp/Reader.java
@@ -1,13 +1,63 @@
 package stability.mot.followUp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class Reader {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	private BufferedReader reader;
+	private FileReader fileReader;
+	private File file;
+	
+	public Reader() throws FileNotFoundException {
+		try {
+			this.file = new File("C:\\Users\\Jefferson\\workspace\\StabilityProject\\src\\Results.txt");
+			this.fileReader = new FileReader(file);
+			this.reader =  new BufferedReader(fileReader);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}		
+	}
+	
 
+	private void readFile() throws IOException {
+		String line;
+        try {
+			while((line = reader.readLine()) != null){
+			   System.out.println(line);
+			   readTombstone(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        reader.close();		
 	}
 
+
+	private void readTombstone(String line) {
+		int numberOfTombstone;
+		if(line.startsWith("Tombstones")){
+			System.out.println("Linha = " + line);
+			String numberString[] = line.split(" ");
+			String n = numberString[1].substring(1, numberString[1].length()-2);
+			numberOfTombstone = Integer.parseInt(n);
+			JOptionPane.showMessageDialog(null, "Linha = " + line + "number = " + numberOfTombstone);
+		}		
+	}
+
+
+	public static void main(String[] args) throws FileNotFoundException{
+		Reader reader = new Reader();
+		try {
+			reader.readFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/stability/mot/followUp/Tombstone.java
+++ src/stability/mot/followUp/Tombstone.java
@@ -5,5 +5,53 @@ public class Tombstone {
 	private String directory;
 	private String reportName;
 	private String testCase;
+	private String tipo;
+	private String pid;
+	private String tid;
+	private String line;
+	
+	public String getDirectory() {
+		return directory;
+	}
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
+	public String getReportName() {
+		return reportName;
+	}
+	public void setReportName(String reportName) {
+		this.reportName = reportName;
+	}
+	public String getTestCase() {
+		return testCase;
+	}
+	public void setTestCase(String testCase) {
+		this.testCase = testCase;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public String getTid() {
+		return tid;
+	}
+	public void setTid(String tid) {
+		this.tid = tid;
+	}
+	public String getLine() {
+		return line;
+	}
+	public void setLine(String line) {
+		this.line = line;
+	}
+	
 	
 }
